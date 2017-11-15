967ff96a281120e2e126eb270ea4c7522d66a182
liujie1986 <happyboy_lj@126.com>
2017-11-15 17:05:31 +0800

    implement hex_asc() and asc_hex()

--- string/string_conv.c
+++ string/string_conv.c
@@ -2,10 +2,40 @@
 
 UINT8 asc_hex(UINT8 *asc, UINT8 *hex, UINT64 len)
 {
+	int i;
+	UINT8 tempstr[2];
+
+	if(!asc || !hex)
+	{
+		return 0;
+	}
+
+	memset(hex, 0, len);
+
+	for(i = 0; i < len; i++)
+	{
+		memcpy(tempstr, asc + i * 2, 2);
+		*hex++ = (UINT8)(strtol(tempstr, NULL, 16));
+	}	
+
 	return 0;
 }
 
 UINT8 hex_asc(UINT8 *hex, UINT8 *asc, UINT64 len)
 {
+	int i;
+
+	if(!asc || !hex)
+	{
+		return 0;
+	}
+
+	memset(asc, 0, len * 2);
+	
+	for(i = 0; i < len; i++)
+	{
+		sprintf(asc + i * 2, "%02X", *hex++);
+	}
+
 	return 0;
 }
